a942cc111e80e25ad914c7cdacf93f21ea2fdad6
danigarcia47 <dgarcial01@educantabria.es>
2022-02-27 10:21:46 +0100

    2 files modified, 15 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Header from './components/Header';
 import BootstrapHeader from './components/BootstrapHeader';
 import { MenuItems } from './data/MenuItems';
 
